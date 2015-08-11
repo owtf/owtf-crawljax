48a7694cd89973556230eeadb0c8ec49718d5a34
delta24 <viyat001@gmail.com>
2015-08-11 15:06:30 +0530

    removed redundant Config class

--- src/org/crawljax_owtf/main/Builder.java
+++ src/org/crawljax_owtf/main/Builder.java
@@ -5,11 +5,14 @@ import com.crawljax.core.CrawljaxRunner;
 import com.crawljax.core.configuration.BrowserConfiguration;
 import com.crawljax.core.configuration.CrawljaxConfiguration;
 import com.crawljax.core.configuration.CrawljaxConfiguration.CrawljaxConfigurationBuilder;
-import com.google.gson.Gson;
+import com.crawljax.plugins.crawloverview.CrawlOverview;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 
@@ -18,48 +21,44 @@ public class Builder {
     public static void main(String[] args) throws Exception {
         System.getProperties().put("http.proxyHost", "127.0.0.1");
         System.getProperties().put("http.proxyPort", "8008");
-        //System.getProperties().put("http.proxyUser", "someUserName");
-        //System.getProperties().put("http.proxyPassword", "somePassword");
 
-        Gson gson = new Gson();
-        Config conf = new Config();
-
+        ObjectMapper mapper = new ObjectMapper();
+        HashMap<String, Object> map = new HashMap<String, Object>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader("/home/viyat/IdeaProjects/owtf-crawljax/config.json"));
-
-            conf = gson.fromJson(reader, Config.class);
+            String message = org.apache.commons.io.IOUtils.toString(reader);
+            map = mapper.readValue(message, HashMap.class);
 
         } catch  (IOException e) {
             e.printStackTrace();
         }
 
-        CrawljaxConfigurationBuilder builder = CrawljaxConfiguration.builderFor(conf.getTarget());
+        CrawljaxConfigurationBuilder builder = CrawljaxConfiguration.builderFor(map.get("target").toString());
 
         builder.crawlRules().insertRandomDataInInputForms(false);
 
         // click these elements
         builder.crawlRules().clickDefaultElements();
 
-        int c = conf.getClickElements().size();
+        int c = ((ArrayList<Object>)map.get("clickElements")).size();
 
         for(int i=0; i < c; i++) {
-            builder.crawlRules().click(conf.getClickElements().get(i));
+            builder.crawlRules().click(map.get("clickElements").toString());
         }
 
-        builder.setMaximumStates(conf.getMaxStates());
-        builder.setMaximumDepth(conf.getDepth());
+        builder.setMaximumStates((int)map.get("maxStates"));
+        builder.setMaximumDepth((int)map.get("depth"));
         builder.crawlRules().clickElementsInRandomOrder(true);
 
         // Set timeouts
-        builder.crawlRules().waitAfterReloadUrl(conf.getWaitAfterReload(), TimeUnit.MILLISECONDS);
-        builder.crawlRules().waitAfterEvent(conf.getWaitAfterEvent(), TimeUnit.MILLISECONDS);
+        builder.crawlRules().waitAfterReloadUrl((int)map.get("waitAfterReload"), TimeUnit.MILLISECONDS);
+        builder.crawlRules().waitAfterEvent((int)map.get("waitAfterEvent"), TimeUnit.MILLISECONDS);
 
         // use two browsers simultaneously.
         builder.setBrowserConfig(new BrowserConfiguration(BrowserType.FIREFOX, 2));
-        //builder.addPlugin(new CrawlOverview());
+        builder.addPlugin(new CrawlOverview());
 
         CrawljaxRunner crawljax = new CrawljaxRunner(builder.build());
         crawljax.call();
     }
 }
-
