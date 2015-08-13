770f1fe0ad29e0755c4f8a1729983c23ba7df90c
delta24 <viyat001@gmail.com>
2015-08-13 21:37:21 +0530

    Moved webapp integration to another branch on the main repo

--- webapp/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# starts the crawljax webapp
-curr=`dirname $0`
-host=127.0.0.1
-proxy_port=8008
-port=$1
-
-java -Dhttp.proxyHost=${host} -Dhttp.proxyPort=${proxy_port} -jar crawljax-web-3.6.jar -p ${port}
